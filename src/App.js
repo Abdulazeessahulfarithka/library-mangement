b985aa89ebc461d67c5f44b281a1368e2be9c087
“Abdul <kaazees31@gmail.com>
2022-12-31 15:34:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import  "./style.css";
+import "./style.css";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}>
-          <Route path="/" element={<Login />}/>
+            <Route path="/" element={<Login />} />
           </Route>
-
         </Routes>
       </BrowserRouter>
     </>
